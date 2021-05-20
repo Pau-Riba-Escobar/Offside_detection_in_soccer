58a343f0f6fe9c4401005f842760a7c130ac138e
Pr-1529314 <pau.ribae@e-campus.uab.cat>
2021-05-20 20:02:29 +0200

    geometric utils updated

--- src/geometric_utils.py
+++ src/geometric_utils.py
@@ -1,4 +1,12 @@
 import numpy as np
+import cv2
+
+def draw_lines(im, lines):
+    for l in lines:
+        r,t = l[0]
+        p1,p2 = hough_to_projective(r,t)
+        cv2.line(im,p1,p2,(255,0,0),1)
+    return im
 
 def hough_to_projective(ro,theta):
     # sinus y cosinus
@@ -18,8 +26,28 @@ def intersection_point(l1_eq,l2_eq):
     p1 = np.cross(l1_eq,l2_eq) # p1 = [0,0,0] ----> no hay intersección entre las rectas
     return p1/p1[2]# dividimos ya que tenemos un espacio tridimensional(proyectivo) y así lo pasamos a proyectivo 
 
+
+def get_lines(im):
+    # list to hold the  selected lines
+    ultimate_lines = []
+    # thresholds for the lines
+    r_thres = 100
+    # Canny to get the edges
+    edges = cv2.Canny(cv2.cvtColor(im,cv2.COLOR_RGB2GRAY), 150, 200, 3)
+    # then we will be applying hough transform to the edged image in order to obtain the lines
+    lines = cv2.HoughLines(edges,1,np.pi/180, 200) # we only take the lines that have over 200 points
+    # we take the first line as a reference
+    reference_line = lines[0]
+    ultimate_lines.append(reference_line)
+    for l in lines[1:]:
+        r,t = l[0]
+        if abs(r-reference_line[0][0]) > r_thres:# reference_line[0][0] = ro
+            ultimate_lines.append(l)
+    return ultimate_lines
+
 def get_vanishing_point(lines):
     # A partir de las diferentes lineas obtenidas con Hough obtendremos el punto de Fuga
+    pass
     """
     vanishing_points = []
     for line1 in lines:
--- src/main.py
+++ src/main.py
@@ -1,7 +1,10 @@
 import numpy as np
 import os
 import cv2
+from numpy.lib import utils
+from geometric_utils import *
 
+"""
 def intersection(lines, points):
     for l in lines:
        x = (l[0][0][0] - l[0][1][0], points[0][0]-points[1][0])
@@ -15,8 +18,8 @@ def intersection(lines, points):
 # NOMÉS TINDREM EN COMPTE LÍNIES VERTICALS
 def get_lines(im, lines, orientation):
     if orientation == "right":
-        max_angle =  140
-        min_angle = 100
+        max_angle =  150
+        min_angle = 105
     else:
         max_angle =  70
         min_angle = 30
@@ -32,16 +35,17 @@ def get_lines(im, lines, orientation):
         angle = t*180/np.pi
         # si theta se encuentra dentro del rango indicado
         if  angle > min_angle and angle < max_angle:
-                s,c = [np.sin(t),np.cos(t)]
-                # Central point of the line
-                xb,yb = [c*r, s*r]
-                # Necessitem dos punts extrems per a construir la línia
-                p1 = [int(xb + 10000*(-s)),int(yb + 10000*c)]
-                p2 = [int(xb - 10000*(-s)),int(yb - 10000*c)]
-                good_lines.append([[p1,p2],[r,t]])
-                cv2.line(im,(p1[0],p1[1]),(p2[0],p2[1]),(255,0,0),1)
-                break
+            s,c = [np.sin(t),np.cos(t)]
+            # Central point of the line
+            xb,yb = [c*r, s*r]
+            # Necessitem dos punts extrems per a construir la línia
+            p1 = [int(xb + 10000*(-s)),int(yb + 10000*c)]
+            p2 = [int(xb - 10000*(-s)),int(yb - 10000*c)]
+            good_lines.append([[p1,p2],[r,t]])
+            cv2.line(im,(p1[0],p1[1]),(p2[0],p2[1]),(255,0,0),1)
+            break
     
+    rthd = 50
     # CAS DE LA SEGONA LÍNEA
     
     for l in lines[last_line+1:]:
@@ -51,29 +55,29 @@ def get_lines(im, lines, orientation):
         angle = t*180/np.pi
         # si theta se encuentra dentro del rango indicado
         if  angle > min_angle and angle < max_angle:
-                s,c = [np.sin(t),np.cos(t)]
-                # Central point of the line
-                xb,yb = [c*r, s*r]
-                # Necessitem dos punts extrems per a construir la línia
-                p1 = [int(xb + 10000*(-s)),int(yb + 10000*c)] # point 1=(x1,y1)
-                p2 = [int(xb - 10000*(-s)),int(yb - 10000*c)] # point 2=(x2,y2)
-                intersected, intersected_line = intersection(good_lines,[p1,p2])
-                # Si les línies han intersectat i estàn suficientment separades(no volem linies quasi coincidents)
-                if intersected and abs(intersected_line[1][0] - r) > 50:
-                    good_lines.append([[p1,p2],[r,t]])
-                    cv2.line(im,(p1[0],p1[1]),(p2[0],p2[1]),(255,0,0),1)
-                    break
+            s,c = [np.sin(t),np.cos(t)]
+            # Central point of the line
+            xb,yb = [c*r, s*r]
+            # Necessitem dos punts extrems per a construir la línia
+            p1 = [int(xb + 10000*(-s)),int(yb + 10000*c)] # point 1=(x1,y1)
+            p2 = [int(xb - 10000*(-s)),int(yb - 10000*c)] # point 2=(x2,y2)
+            intersected, intersected_line = intersection(good_lines,[p1,p2])
+            # Si les línies han intersectat i estàn suficientment separades(no volem linies quasi coincidents)
+            if intersected and abs(intersected_line[1][0] - r) > 50:
+                good_lines.append([[p1,p2],[r,t]])
+                cv2.line(im,(p1[0],p1[1]),(p2[0],p2[1]),(255,0,0),1)
+                break
     # image with lines
     cv2.imshow("lines of the image", im)
     cv2.waitKey(0)
     return good_lines
 
-orientation = "right"
+orientation = "left"
 # set de imatges
 imset = ["../Dataset/"+file for file in os.listdir("../Dataset/")]
 
 # imatge d'exemple
-im = cv2.imread(imset[0])
+im = cv2.imread(imset[11])
 # old_shape = (cv2.resize(im,(int(im.shape[0]/2), int(im.shape[1]/2)))).shape
 new_size = (int(im.shape[0]/2), int(im.shape[1]/2))
 # imatge més petita per a un millor processament
@@ -81,15 +85,22 @@ im = cv2.resize(im, new_size)
 # Imatge amb HSV per a obtenir millor els edges, ja que les línies blanques no es veuen gaire bé en algunes imatges
 hsv_im = cv2.cvtColor(im, cv2.COLOR_BGR2HSV)
 # edges = cv2.Canny(hsv_im, 150, 250, apertureSize=3)
-"""# Creem una imatge més gran per a visualitzar el punt de fuga
-large_im = np.ones(old_shape, dtype=np.uint8) * 255
-large_im[(large_im.shape[0]-hsv_im.shape[0]):, 0:(large_im.shape[1]-hsv_im.shape[1]), :] = hsv_im"""
 edges = cv2.Canny(hsv_im, 150, 250, apertureSize=5)
 tmp_lines = cv2.HoughLines(edges,1,np.pi/180,200)
 lines = get_lines(im, tmp_lines, orientation)
+"""
+# image set
+imset = ["../Dataset/"+file for file in os.listdir("../Dataset/")]
+test_im = cv2.imread(imset[3])
+# resizing the image for testing to half of its size
+test_im = cv2.resize(test_im,(int(test_im.shape[0]/2), int(test_im.shape[1]/2)))
+# Now we have to apply hough transform to obtain the lines of the image
+lines = np.array(get_lines(test_im))
 
-
-
+# to watch what lines we have found let's draw them in our test image and show them
+im = draw_lines(test_im,lines)
+cv2.imshow("lined image", im)
+cv2.waitKey(0)
 
 
 
