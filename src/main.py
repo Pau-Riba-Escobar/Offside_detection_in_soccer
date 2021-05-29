a27fe4d5dcd06fc485ab308359b860662dfa997c
Pr-1529314 <pau.ribae@e-campus.uab.cat>
2021-05-29 10:10:37 +0200

    segmentation finished

--- src/main.py
+++ src/main.py
@@ -8,7 +8,7 @@ from segmentation_utils import *
 # image set
 imset = ["DataSet/Train/"+file for file in os.listdir("DataSet/Train/")]
 
-test_im = cv2.imread(imset[3])
+test_im = cv2.imread(imset[0])
 # resizing the image for testing to half of its size
 test_im = cv2.resize(test_im,(int(test_im.shape[0]/2), int(test_im.shape[1]/2)))
 # Now we have to apply hough transform to obtain the lines of the image
@@ -18,7 +18,7 @@ vp = get_vanishing_point(lines)
 
 # to watch what lines we have found let's draw them in our test image and show them
 # im = draw_lines(test_im,lines) UNCOMMENT TO SEE THE LINES IN THE IMAGE
-thr = extract_players(test_im)
-show_contours(test_im.copy(),thr)
+thr = extract_playersv2(test_im)
+# show_contours(test_im.copy(),thr)
 
 
--- src/segmentation_utils.py
+++ src/segmentation_utils.py
@@ -1,8 +1,7 @@
 import numpy as np
 import cv2
+from matplotlib import pyplot as plt
 
-#converting into hsv image
-#green range
 lower_green = np.array([36,0,0])
 upper_green = np.array([86, 255, 255])
 
@@ -13,16 +12,20 @@ closing_k = np.ones((40,40),np.uint8)
 erode_k = np.ones((15,1),np.uint8)
 dilate_k = np.ones((10,10),np.uint8)
 
-def extract_players(im):
+# function that extracts the players, the gk and the referee using a preset range of green as a mask for field segmentation
+def extract_playersV1(im):
     # hsv
     hsv = cv2.cvtColor(im,cv2.COLOR_BGR2HSV)
     grass_mask = cv2.inRange(hsv, lower_green, upper_green)
     cv2.imshow('green_mask',grass_mask)
     cv2.waitKey(0)
+    # FER CLOSING PER TAPAR ELS JUGADORS
+    # RESTAR ELS JUGADORS DE LA IMATGE INICIAL
     mask = 255 - grass_mask
     thresh_players = cv2.morphologyEx(mask, cv2.MORPH_CLOSE, player_k)
     cv2.imshow('players',thresh_players)
     cv2.waitKey(0)
+    
     thresh_aud = cv2.morphologyEx(mask, cv2.MORPH_CLOSE, audience_k)
     cv2.imshow('aud',thresh_aud)
     cv2.waitKey(0)
@@ -36,6 +39,53 @@ def extract_players(im):
     cv2.imshow('thresh',thresh)
     cv2.waitKey(0)
     return thresh
+
+def getHueThresh(hsv_im):
+    # calculating a histogram of hue component( color)
+    hist = cv2.calcHist([hsv_im], [0],mask = None, histSize=[360], ranges=[0,360]) # histSzie= number of bins
+    # taking only the interesting part of it
+    max_val = np.argmax(hist) 
+    init_l = max_val
+    init_r = max_val
+    diff = np.inf
+    dev = 0
+    while diff > 0:
+        diff = (hist[init_l] - hist[init_l - 1]) + (hist[init_r] - hist[init_r + 1]) 
+        init_l -= 1
+        init_r += 1
+        dev += 1
+    # plt.plot(hist[max_val - dev:max_val + dev], color="g")
+    plt.plot(hist, color="g")
+    plt.show()
+    return np.array([max_val-dev,0,0]),np.array([max_val+dev, 255, 255])
+
+def extract_playersv2(im):
+    # im to hsv to extract hue info
+    hsv = cv2.cvtColor(im,cv2.COLOR_RGB2HSV)
+    # getting a threshold of the hue of the image and taking the largest component as grass
+    lower_bound, upper_bound  = getHueThresh(hsv)
+    grass_mask = cv2.inRange(hsv, lower_bound, upper_bound)
+    # dilating to remove lines noise
+    gmask_dilated = cv2.morphologyEx(grass_mask, cv2.MORPH_DILATE, np.ones((3,3)))
+    # gmask_dilated = cv2.morphologyEx(gmask_dilated, cv2.MORPH_ERODE, np.ones((15,30)))
+    cv2.imshow("grass mask applied", gmask_dilated)
+    # cv2.waitKey(0)
+    audience_open = cv2.morphologyEx(grass_mask,cv2.MORPH_CLOSE,np.ones((15,15)))
+    audience = cv2.morphologyEx(audience_open,cv2.MORPH_OPEN,np.ones((70,70)))
+    pitch = 255 - audience
+    cv2.imshow("audience out field in", pitch)
+    # bitwise and to put together both masks
+    m = cv2.bitwise_and(audience, gmask_dilated)
+    m = cv2.bitwise_or(m,pitch)
+    cv2.imshow("clean mask", m)
+    cv2.waitKey(0)
+    return m 
+    # REGION PROPS
+    # nlabels,labels,stats,centroids = cv2.connectedComponentsWithStats(255-tmp)
+    # bg_mask = (labels == 0).astype("uint8")*255
+    # tmp2 = cv2.bitwise_and(tmp,bg_mask)
+    # removing the audience with an opening
+    
 
 def show_contours(im, thresh): 
     contours, hierarchy = cv2.findContours(thresh,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
